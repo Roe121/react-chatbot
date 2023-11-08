d849a177a045b401b50983884889df149e790e55
Roe Lez <lezzarrami@gmail.com>
2023-11-08 18:42:17 +0100

    ActionProvider, config, MessageParser files add

---
+++ src/config.js
@@ -0,0 +1,7 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  initialMessages: [createChatBotMessage(`Hi there, how can i help you ?`)]
+}
+
+export default config
