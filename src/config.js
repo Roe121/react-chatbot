02be18d5364899bf431ae29f16ab9af00d53731c
Roe Lez <lezzarrami@gmail.com>
2023-11-08 19:27:33 +0100

    Styles config file update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,16 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hi there, how can i help you ?`)]
+  botName: "LearningBot" ,
+  initialMessages: [createChatBotMessage(`Hi there, how can i help you ?`)],
+  customStyles: {
+    botMessageBox: {
+        backgroundColor: "#376B7E" ,
+    },
+    chatButton: {
+        backgroundColor: "#376B7E", 
+    },
+  },
 }
 
 export default config
